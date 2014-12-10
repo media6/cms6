1d3f37efee2786837cc5db77b40a44ebf7c7bc89
dabelzee <trembmat@gmail.com>
2014-12-10 00:56:39 -0500

    first load problem corrected

--- inc/init_db.php
+++ inc/init_db.php
@@ -1,22 +1,30 @@
 <?
-                              
-                               
-  
 if($my_db_host =="" || $my_db_user=="" || $my_db_pass=="" || $my_db_name=="") {
       print "Erreur de configuration!";
+ $_SESSION = null;
+
 } else {
 
   $db_link = new  iptDbLink($my_db_host,$my_db_user ,$my_db_pass);
   $active_db = new iptDb($db_link,$my_db_name);
   $active_db->Create();
-  
-  if(!$active_db->DbObject()) {
-       print "Connexion impossible à la base de données!";
- 
-  } else {
-  	include_once(realpath(dirname(__FILE__))."/../classes/classes.php");
-  	include_once("login.php");
-  }
+
+	if(!$active_db->Connect()) {
+
+		 print "<br>Connexion impossible au serveur de donnees";
+		$_SESSION = null;
+		die;
+	} else {
+	    if(!$active_db->DbObject()) {
+	       print "<br>Connexion impossible à la base de données!";
+		 $_SESSION = null;
+
+		die;
+	    } else {
+	  	include_once(realpath(dirname(__FILE__))."/../classes/classes.php");
+	  	include_once("login.php");
+ 	    }
+	}
 
 }
 
